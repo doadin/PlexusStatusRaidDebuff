0bb98d8e37cfe7bd9f9d9ebe44a84d0c1d08ecbd
stassart <stassart@7773b974-e9da-46fa-9fc0-750de12464ba>
2012-07-30 03:48:05 +0000

    Added Locale files, building with Curseforge translation.

---
+++ Locale/enUS.lua
@@ -0,0 +1,9 @@
+local ignore_missing = true
+--@debug@
+ignore_missing = false
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("GridStatusRaidDebuff", "enUS", true, ignore_missing)
+
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none")@
+
---
+++ Locale/zhTW.lua
@@ -0,0 +1,9 @@
+local ignore_missing = true
+--@debug@
+ignore_missing = false
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("GridStatusRaidDebuff", "zhTW", false, ignore_missing)
+
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none")@
+
