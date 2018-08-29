33db2c572b4fca2093b1d10f1475d214da03563f
tptbk <tptbk@7773b974-e9da-46fa-9fc0-750de12464ba>
2018-08-29 17:13:32 +0000

    Better Fixes for Debuff Detection

--- Core.lua
+++ Core.lua
@@ -268,12 +268,13 @@ function GridStatusRaidDebuff:UpdateAllUnits()
 end
 
 function GridStatusRaidDebuff:ScanNewDebuff(unitid, guid)
-    local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing = CombatLogGetCurrentEventInfo()
-	if not spellName then return end
+    local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, name, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing = CombatLogGetCurrentEventInfo()
+	if not name then return end
+    if name == -1 then return end
 	local settings = self.db.profile["alert_RaidDebuff"]
 	if (settings.enable and debuff_list[realzone]) then
 		if sourceGUID and not GridRoster:IsGUIDInGroup(sourceGUID) and GridRoster:IsGUIDInGroup(destGUID)
-			and not debuff_list[realzone][spellName] then
+			and not debuff_list[realzone][name] then
 			if ignore_ids[spellId] then return end --Ignore Dazed
 
 			-- Filter out non-debuff effects, only debuff effects are shown
@@ -285,18 +286,18 @@ function GridStatusRaidDebuff:ScanNewDebuff(unitid, guid)
 			    if (UnitDebuff(unitid, i)) then
 			    	debuff = true
 			     else
-			     	self:Debug("Debuff not found", spellName)
+			     	self:Debug("Debuff not found", name)
 			    end
             end
 			if not debuff then return end
 
-			self:Debug("New Debuff", sourceName, destName, spellName, unitid, tostring(debuff))
+			self:Debug("New Debuff", sourceName, destName, name, unitid, tostring(debuff))
 
-			self:DebuffLocale(realzone, spellName, spellId, 5, 5, true, true)
+			self:DebuffLocale(realzone, name, spellId, 5, 5, true, true)
 			if not self.db.profile.detected_debuff[realzone] then self.db.profile.detected_debuff[realzone] = {} end
-			if not self.db.profile.detected_debuff[realzone][spellName] then self.db.profile.detected_debuff[realzone][spellName] = spellId end
+			if not self.db.profile.detected_debuff[realzone][name] then self.db.profile.detected_debuff[realzone][name] = spellId end
 
-            self:LoadZoneDebuff(realzone, spellName)
+            self:LoadZoneDebuff(realzone, name)
 			
 		end
 	end
@@ -550,12 +551,12 @@ function GridStatusRaidDebuff:LoadZoneDebuff(zone, name)
 	local args = self.options.args[zone].args
 
 	-- Code by Mikk
-    if debuff_list[zone] and debuff_list[zone][name] then
-	    k = debuff_list[zone][name]
-    end
-    if k then
-	    local order = k.order
-    end
+
+	k = debuff_list[zone][name]
+
+
+	local order = k.order
+
 	-- Make it sorted by name. Values become 9999.0 -- 9999.99999999
 	if order==9999 then
 		local a,b,c = string.byte(name, 1, 3)
